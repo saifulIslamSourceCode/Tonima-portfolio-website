121bae5739fb47611316e7e465005e1325c27b50
Saiful Islam <76849761+saifulIslamSourceCode@users.noreply.github.com>
2025-04-18 19:23:35 +0600

    change work section
    add zoom effect of the model images

--- javaScript/javascript.js
+++ javaScript/javascript.js
@@ -20,7 +20,7 @@ const slider = document.getElementById('slider');
   //nav links text will be black after 100 vh
   window.addEventListener("scroll", () =>{
     let navLinks = document.querySelectorAll('.nav');
-    let targetHeight = window.innerHeight;
+    let targetHeight = window.innerHeight * 1.6;
 
     if(window.scrollY >= targetHeight) {
       navLinks.forEach(links => links.classList.add("black"));
@@ -29,3 +29,50 @@ const slider = document.getElementById('slider');
 
     }
   })
+
+
+  // Work section add displayNan for hover effect
+  let imgConteiner_1 = document.getElementById("dresses_div_1");
+  let imgConteiner_2 = document.getElementById("dresses_div_2");
+  let imgConteiner_3 = document.getElementById("dresses_div_3");
+  let imgConteiner_4 = document.getElementById("dresses_div_4");
+
+  function zoomEvent(container) {
+
+    let img = container.getElementsByTagName("img")[0];
+    let scale = 2;
+
+    // Move over image
+    img.addEventListener('mousemove', (e) => {
+      const rect = img.getBoundingClientRect();
+      const x = ((e.clientX - rect.left) / rect.width) * 100;
+      const y = ((e.clientY - rect.top) / rect.height) * 100;
+
+      img.style.transformOrigin = `${x}% ${y}%`;
+      img.style.transform = `scale(${scale})`;
+    });
+
+    // // Zoom in/out with mouse wheel over image
+    // img.addEventListener('wheel', (e) => {
+    //   e.preventDefault();
+    //   scale += e.deltaY * -0.001;
+    //   scale = Math.min(Math.max(1, scale), 3);
+    //   img.style.transform = `scale(${scale})`;
+    // });
+
+    // Reset only when mouse leaves the image
+    img.addEventListener('mouseleave', () => {
+      scale = 2;
+      img.style.transform = 'scale(1)';
+    });
+
+
+
+
+
+  }
+
+  zoomEvent(imgConteiner_1);
+  zoomEvent(imgConteiner_2);
+  zoomEvent(imgConteiner_3);
+  zoomEvent(imgConteiner_4);
